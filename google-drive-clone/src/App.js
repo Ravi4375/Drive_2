12a4f883fc3298766e74accd25da0662deba2ec3
Ravi Dhakad <ravidhakad4375@gmail.com>
2026-02-10 14:03:39 +0530

    Add files via upload

---
+++ google-drive-clone/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DriveApp from "./DriveApp";
+
+
+import "../src/component/FileCard.css";
+function App() {
+  return (
+    <div className="App">
+      <DriveApp />
+    </div>
+  );
+}
+
+export default App;
